ccb77ac14ba8f9c9bb61893446e2f3adafe9d73e
Max Anderson <m.d.anderson2@student.vu.nl>
2023-12-15 00:15:23 +0100

    3D indexing for Q-values, and printing

--- maze.py
+++ maze.py
@@ -1,14 +1,13 @@
 import numpy as np
-import cProfile
 
 MAZE_SIZE = 5
-ALPHA_LEARNING = 0.1
-EPSILON_CONVERGENCE = 0.1
+ALPHA_LEARNING = 0.2
+EPSILON_EXPLORATION = 0.4
 GAMMA_DISCOUNT = 0.9
 
 ACTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # up, down, left, right
 
-class MazeEnvironment:
+class Maze:
     def __init__(self, start=(0, 0), goal=(4, 4), reward=1.0):
         self.start = start
         self.goal = goal
@@ -35,7 +34,7 @@ class MazeEnvironment:
         for i in range(MAZE_SIZE):
             for j in range(MAZE_SIZE):
                 if (i, j) == self.state:
-                    print('@', end=' ')
+                    print('o', end=' ')
                 elif (i, j) == self.goal:
                     print('X', end=' ')
                 else:
@@ -45,56 +44,43 @@ class MazeEnvironment:
 class TabularQLearningAgent:
     def __init__(self, maze_env):
         self.maze_env = maze_env
-        self.q_table = np.zeros((MAZE_SIZE**2, len(ACTIONS)))
+        self.q_table = np.zeros((MAZE_SIZE, MAZE_SIZE, len(ACTIONS)))
 
-    def learn(self, episodes=100):
+    def learn(self, episodes=10000):
         for _ in range(episodes):
             state = self.maze_env.reset()
             done = False
 
             while not done:
-                current_state_index = self.get_state_index(state)
-                action_index = self.select_action(current_state_index)
+                action_index = self.select_action(state)
                 next_state, reward, done = self.maze_env.step(ACTIONS[action_index])
-                self.update_q_table(current_state_index, action_index, reward, next_state)
+                self.update_q_table(state, action_index, reward, next_state)
                 state = next_state
 
-    def get_state_index(self, state):
-        return state[0] * MAZE_SIZE + state[1]
-
     def select_action(self, state_index):
-        if np.random.rand() < EPSILON_CONVERGENCE:
+        if np.random.rand() < EPSILON_EXPLORATION:
             return np.random.randint(len(ACTIONS))
         else:
-            return np.argmax(self.q_table[state_index])
+            return np.argmax(self.q_table[state_index[0], state_index[1]])
 
     def update_q_table(self, state, action, reward, next_state):
-        next_state_index = self.get_state_index(next_state)
+        next_max = np.max(self.q_table[next_state[0], next_state[1]])
 
-        # Get maximum Q-value for the next state
-        next_max = np.max(self.q_table[next_state_index])
+        # Calculate new Q-value
+        self.q_table[state[0], state[1], action] *= (1 - ALPHA_LEARNING)
+        self.q_table[state[0], state[1], action] += ALPHA_LEARNING * (reward + GAMMA_DISCOUNT * next_max)
 
-        # Update Q-value
-        self.q_table[state, action] *= (1 - ALPHA_LEARNING)
-        self.q_table[state, action] += ALPHA_LEARNING * (reward + GAMMA_DISCOUNT * next_max)
+    def print_q_table(self):
+        print(f"                 up     down    left    right")
+        for row in range(MAZE_SIZE):
+            for col in range(MAZE_SIZE):
+                q_values_formatted = ["{:.2f}".format(q) for q in self.q_table[row, col]]
+                print(f"State ({row}, {col}): {q_values_formatted}")
 
-def main():
-    maze_env = MazeEnvironment()
-    agent = TabularQLearningAgent(maze_env)
-    agent.learn()
+maze = Maze()
+agent = TabularQLearningAgent(maze)
+agent.learn()
 
-    # Inspect the learned Q-table
-    print(agent.q_table)
-
-cProfile.run('main()', 'profile_stats')
-import pstats
-p = pstats.Stats('profile_stats')
-p.sort_stats('cumulative').print_stats(10)
-# # Example usage
-# maze_env = MazeEnvironment()
-# print("Initial state:", maze_env.reset())
-# maze_env.print_maze()
-
-# next_state, reward, done = maze_env.step('right')
-# print("Next state:", next_state, "Reward:", reward, "Done:", done)
-# maze_env.print_maze()
+maze.reset()
+maze.print_maze()
+agent.print_q_table()
