d49e01149f8e4690efe33ce2696552b6057f1ef3
Max Anderson <m.d.anderson2@student.vu.nl>
2023-12-28 13:29:20 +0100

    Simpler logic to remove variables/match algorithm

--- maze.py
+++ maze.py
@@ -11,7 +11,7 @@ ACTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # up, down, left, right
 class Maze:
     def __init__(self, start=(0, 0), goal_states = {(MAZE_SIZE - 1, MAZE_SIZE - 1): 1.0}):
         self.start = start
-        self.goal_states = goal_states
+        self.goal_state_rewards = goal_states
         self.state = start
 
     def reset(self):
@@ -20,24 +20,24 @@ class Maze:
 
     def step(self, action):
         # Check if the goal is reached
-        if self.state in self.goal_states:
-            return None, self.goal_states[self.state], True
+        if self.state in self.goal_state_rewards:
+            return self.goal_state_rewards[self.state], None
 
         # Calculate the next state
-        next_state = (max(min(self.state[0] + action[0], MAZE_SIZE - 1), 0),
+        next_state = (max(min(self.state[0] + action[0], MAZE_SIZE - 1), 0),    # max(min()) clamps values: attempting to leave maze bounds just wastes a turn
                       max(min(self.state[1] + action[1], MAZE_SIZE - 1), 0))
 
         # Update location
         self.state = next_state
 
-        return next_state, 0, False
+        return 0, next_state
 
     def print_maze(self):
         for i in range(MAZE_SIZE):
             for j in range(MAZE_SIZE):
                 if (i, j) == self.state:
                     print('o', end=' ')
-                elif (i, j) in self.goal_states:
+                elif (i, j) in self.goal_state_rewards:
                     print('X', end=' ')
                 else:
                     print('.', end=' ')
@@ -52,13 +52,12 @@ class Agent:
     def learn(self, episodes=NUM_EPISODES):
         for _ in range(episodes):
             state = self.maze_env.reset()
-            done = False
 
-            while not done:
+            while state is not None:
                 action_index = self.select_action(state)
-                next_state, reward, done = self.maze_env.step(ACTIONS[action_index])
-                self.update_q_table(state, action_index, reward, next_state)
-                state = next_state
+                old_state = state
+                reward, state = self.maze_env.step(ACTIONS[action_index])
+                self.update_q_table(old_state, action_index, reward, state)
 
     def select_action(self, state_index):
         if np.random.rand() < EPSILON_EXPLORATION:
@@ -66,18 +65,15 @@ class Agent:
         else:
             return np.argmax(self.q_table[state_index[0], state_index[1]])
 
-    def update_q_table(self, state, action_index, reward, next_state):
-        if next_state is None:  # If the next state is terminal, only consider the immediate reward
-            q_target = reward
-        else:                   # Otherwise, consider both the immediate reward and discounted future reward
-            next_max = np.max(self.q_table[next_state[0], next_state[1]])
-            q_target = reward + GAMMA_DISCOUNT * next_max
+    def update_q_table(self, old_state, action_index, reward, state):
+        if state is not None:   # If not in terminal state, consider both the immediate reward and discounted future reward
+            reward += GAMMA_DISCOUNT * np.max(self.q_table[state[0], state[1]])
 
-        # Update the Q-value
-        self.q_table[state[0], state[1], action_index] *= (1 - ALPHA_LEARNING)
-        self.q_table[state[0], state[1], action_index] += ALPHA_LEARNING * q_target
+        # Update the Q-value of the state we just left
+        self.q_table[old_state[0], old_state[1], action_index] *= (1 - ALPHA_LEARNING)
+        self.q_table[old_state[0], old_state[1], action_index] += ALPHA_LEARNING * reward
 
-    def print_q_values(self):
+    def print_q_table(self):
         print("Maze Q-Values:")
         for i in range(MAZE_SIZE):
             # First line for 'up' values
@@ -100,13 +96,6 @@ class Agent:
             print("\n")  # Extra newline for spacing between rows
         print()
 
-    def print_q_table(self):
-        print(f"                 up     down    left    right")
-        for row in range(MAZE_SIZE):
-            for col in range(MAZE_SIZE):
-                q_values_formatted = ["{:.2f}".format(q) for q in self.q_table[row, col]]
-                print(f"State ({row}, {col}): {q_values_formatted}")
-
 goal_states = {
                 (MAZE_SIZE - 1, MAZE_SIZE - 1): 1.0,
                 # (MAZE_SIZE - 1, 0)            : 0.5
@@ -118,4 +107,4 @@ agent.learn()
 
 maze.reset()
 maze.print_maze()
-agent.print_q_values()
+agent.print_q_table()
