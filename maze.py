1f1003fdff5b3d5645455a88271f76e4184e39d0
Max Anderson <m.d.anderson2@student.vu.nl>
2023-12-28 14:31:13 +0100

    Comments

--- maze.py
+++ maze.py
@@ -11,38 +11,40 @@ GAMMA_DISCOUNT = 0.9
 ACTIONS = [(1, 0), (-1, 0), (0, -1), (0, 1)]  # up, down, left, right
 
 class Maze:
-    def __init__(self, start=(0, 0), goal_states = {(MAZE_SIZE - 1, MAZE_SIZE - 1): 1.0}):
+    def __init__(self, start=(0, 0), goal_state_rewards = {(MAZE_SIZE - 1, MAZE_SIZE - 1): 1.0}):
         self.start = start
-        self.goal_state_rewards = goal_states
+        self.goal_state_rewards = goal_state_rewards
         self.state = start
 
+    # Reset the maze to the start state
     def reset(self):
         self.state = self.start
         return self.state
 
+    # Execute a step in the maze based on the given action
     def step(self, action):
         # Check if the goal is reached
         if self.state in self.goal_state_rewards:
-            return self.goal_state_rewards[self.state], None
+            return self.goal_state_rewards[self.state], None    # Return the reward and indicate the episode is finished
 
-        # Calculate the next state
-        next_state = (max(min(self.state[0] + action[0], MAZE_SIZE - 1), 0),    # max(min()) clamps values: attempting to leave maze bounds just wastes a turn
+        # Calculate the next state while ensuring it stays within maze bounds
+        next_state = (max(min(self.state[0] + action[0], MAZE_SIZE - 1), 0),    # max(min()) to clamp values: attempting to leave maze bounds just wastes a turn
                       max(min(self.state[1] + action[1], MAZE_SIZE - 1), 0))
 
         # Update location
         self.state = next_state
 
-        return 0, next_state
+        return 0, next_state        # Return a reward of 0, and the calculated next_state
 
     def print_maze(self):
         print("a = agent, X = goal")
         for i in reversed(range(MAZE_SIZE)):
             for j in range(MAZE_SIZE):
-                if (i, j) == self.state:
+                if (i, j) == self.state:                    # Agent's current location
                     print('a', end=' ')
-                elif (i, j) in self.goal_state_rewards:
+                elif (i, j) in self.goal_state_rewards:     # Goal state
                     print('X', end=' ')
-                else:
+                else:                                       # Empty cell
                     print('.', end=' ')
             print()
         print()
@@ -51,11 +53,11 @@ class Agent:
     def __init__(self, maze_env, epsilon_initial = EPSILON_INITIAL):
         self.maze_env = maze_env
         self.q_table = np.zeros((MAZE_SIZE, MAZE_SIZE, len(ACTIONS)))
-        self.epsilon = epsilon_initial
+        self.epsilon_exploration = epsilon_initial
 
     def learn(self, episodes=NUM_EPISODES):
         for _ in range(episodes):
-            self.epsilon = max(EPSILON_MIN, self.epsilon * EPSILON_DECAY)
+            self.epsilon_exploration = max(EPSILON_MIN, self.epsilon_exploration * EPSILON_DECAY)
             state = self.maze_env.reset()
 
             while state is not None:
@@ -65,16 +67,16 @@ class Agent:
                 self.update_q_table(old_state, action_index, reward, state)
 
     def select_action(self, state_index):
-        if np.random.rand() < self.epsilon:
+        if np.random.rand() < self.epsilon_exploration:
             return np.random.randint(len(ACTIONS))
         else:
             return np.argmax(self.q_table[state_index[0], state_index[1]])
 
     def update_q_table(self, old_state, action_index, reward, state):
-        if state is not None:   # If not in terminal state, consider both the immediate reward and discounted future reward
+        if state is not None:   # If not in terminal state, include discounted future reward
             reward += GAMMA_DISCOUNT * np.max(self.q_table[state[0], state[1]])
 
-        # Update the Q-value of the state we just left
+        # Update the Q-value of the state-action we just left
         self.q_table[old_state[0], old_state[1], action_index] *= (1 - ALPHA_LEARNING)
         self.q_table[old_state[0], old_state[1], action_index] += ALPHA_LEARNING * reward
 
@@ -83,22 +85,22 @@ class Agent:
         for i in reversed(range(MAZE_SIZE)):
             # First line for 'up' values
             for j in range(MAZE_SIZE):
-                up_value = "{:.2f}".format(self.q_table[i,j, 0])  # Assuming 'up' is the first action
+                up_value = "{:.2f}".format(self.q_table[i,j, 0])  # 'up' is the first action
                 print(f"    {up_value}     ", end="")
             print()
 
             # Second line for 'left' and 'right' values
             for j in range(MAZE_SIZE):
-                left_value = "{:.2f}".format(self.q_table[i,j, 2])  # Assuming 'left' is the third action
-                right_value = "{:.2f}".format(self.q_table[i,j, 3])  # Assuming 'right' is the fourth action
+                left_value = "{:.2f}".format(self.q_table[i,j, 2])  # 'left' is the third action
+                right_value = "{:.2f}".format(self.q_table[i,j, 3])  # 'right' is the fourth action
                 print(f"{left_value} | {right_value} ", end=" ")
             print()
 
             # Third line for 'down' values
             for j in range(MAZE_SIZE):
-                down_value = "{:.2f}".format(self.q_table[i,j, 1])  # Assuming 'down' is the second action
+                down_value = "{:.2f}".format(self.q_table[i,j, 1])  # 'down' is the second action
                 print(f"    {down_value}     ", end="")
-            print("\n")  # Extra newline for spacing between rows
+            print("\n")
         print()
 
 goal_states = {
@@ -106,10 +108,9 @@ goal_states = {
                 # (MAZE_SIZE - 1, 0)            : 0.5
                 }
 
-maze = Maze(goal_states=goal_states)
+maze = Maze(goal_state_rewards=goal_states)
+maze.print_maze()
+
 agent = Agent(maze)
 agent.learn()
-
-maze.reset()
-maze.print_maze()
 agent.print_q_table()
